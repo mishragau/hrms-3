b1f520a834cf121504673d6ab870921dca3fa66a
Swapnja Gudle <swapnjagudle9@gmail.com>
2021-06-25 22:02:25 +0530

    Modified after the demo of same file at 3 different logical places

---
+++ payment.java
@@ -0,0 +1,7 @@
+This is a program to handle paymnet of my clients
+
+This is a java program
+
+This is the first modification after the commit.
+
+This is the secoknd edit to see that the same file can exist in working dir, staging and repo with different content.
